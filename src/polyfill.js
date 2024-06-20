02ea9d7fb667eeaaebac36e51af05b15e43fd1cf
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-06-20 19:53:25 +0300

    include polyfills for old node

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,5 +1,7 @@
 const findLastIndex = require('array.prototype.findlastindex');
+const fromEntries = require('object.fromentries');
 const replaceAll = require('string.prototype.replaceall');
 
 findLastIndex.shim();
 replaceAll.shim();
+fromEntries.shim();
